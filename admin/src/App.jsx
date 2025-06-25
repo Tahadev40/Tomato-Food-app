4c23da4b6e2ae712c6ce02138e6ff52319dcb751
Taha Khan <tahanawazkhan555@gmail.com>
2025-06-25 17:45:07 +0500

    Fix Navbar path issue

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from './components/Navbar/Navbar';
+import Navbar from './components/navbar/Navbar';
 import Sidebar from './components/Sidebar/Sidebar';
 import { Routes, Route } from 'react-router-dom';
 import Add from './pages/Add/Add';
