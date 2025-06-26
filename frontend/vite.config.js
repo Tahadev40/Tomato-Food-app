fbafe120707eb1b2f51d2c5cbe455275555106f7
Taha Khan <tahanawazkhan555@gmail.com>
2025-06-26 14:00:00 +0500

    fix: one

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { copyFileSync } from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: 'copy-redirects',
-      closeBundle() {
-        copyFileSync('public/_redirects', 'dist/_redirects')
-      }
-    }
-  ]
+  plugins: [react()],
+  
 })
