7ae64e2d3ae101139fe56316238ca90ced70c6e2
Tahadev40 <tahanawazkhan555@gmail.com>
2025-06-26 14:01:51 +0500

    Update Verify.jsx

--- frontend/src/pages/verify/Verify.jsx
+++ frontend/src/pages/verify/Verify.jsx
@@ -15,6 +15,7 @@ const Verify = () => {
 
     const verifyPayment = async () => {
         const response = await axios.post(url+"/api/order/verify",{success,orderId});
+        console.log(response.data);
         if (response.data.success) {
             navigate("/myorders")
         }
